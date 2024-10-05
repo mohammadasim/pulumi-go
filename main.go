1564653215f9680ca54aee0d8bb6d5ab887f617a
Mohammad Asim Ayub <asimayub81@gmail.com>
2024-10-05 18:07:49 +0100

    added iam resources

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ssm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -24,6 +27,40 @@ func main() {
 		if err != nil {
 			return err
 		}
+		// Get lambda execution policy
+		policy, err := iam.LookupPolicy(ctx, &iam.LookupPolicyArgs{
+			Name: pulumi.StringRef("AWSLambdaBasicExecutionRole"),
+		})
+		if err != nil {
+			return err
+		}
+		// Create assume role
+		// firs define the policy as a map
+		assumeRolePolicyMap := map[string]interface{}{
+			"Version": "2012-10-17",
+			"Statement": []map[string]interface{}{
+				{
+					"Effect": "Allow",
+					"Principal": map[string]interface{}{
+						"Service": "lambda.amazonaws.com",
+					},
+					"Action": "sts:AssumeRole",
+				},
+			},
+		}
+		// convert the map to json using json.Marshal
+		assumeRolePolicy, err := json.Marshal(assumeRolePolicyMap)
+		if err != nil {
+			return err
+		}
+
+		// Create IAM role for lambda
+		lambdaRole, err := iam.NewRole(ctx, "goLambdaRole", &iam.RoleArgs{
+			Name:              pulumi.String("goLambdaRole"),
+			AssumeRolePolicy:  pulumi.String(string(assumeRolePolicy)),
+			Description:       pulumi.StringPtr("IAM role for the Golang Lambda Function"),
+			ManagedPolicyArns: pulumi.StringArrayArrayInput(policy.Arn),
+		})
 		// Export the name of the bucket
 		ctx.Export("bucketName", bucket.ID())
 		ctx.Export("ssmName", ssmParm.Name)
