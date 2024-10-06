ce2cb76f962b2fa4b2f6fed70fadbea119ec8da6
Mohammad Asim Ayub <asimayub81@gmail.com>
2024-10-06 17:05:04 +0100

    working example of lambda function

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 
+	"github.com/pulumi/pulumi-archive/sdk/go/archive"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/lambda"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ssm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -56,14 +58,46 @@ func main() {
 
 		// Create IAM role for lambda
 		lambdaRole, err := iam.NewRole(ctx, "goLambdaRole", &iam.RoleArgs{
-			Name:              pulumi.String("goLambdaRole"),
-			AssumeRolePolicy:  pulumi.String(string(assumeRolePolicy)),
-			Description:       pulumi.StringPtr("IAM role for the Golang Lambda Function"),
-			ManagedPolicyArns: pulumi.StringArrayArrayInput(policy.Arn),
+			Name:             pulumi.String("goLambdaRole"),
+			AssumeRolePolicy: pulumi.String(string(assumeRolePolicy)),
+			Description:      pulumi.StringPtr("IAM role for the Golang Lambda Function"),
 		})
+		if err != nil {
+			return err
+		}
+		// Attach the policy to the role
+		_, err = iam.NewRolePolicyAttachment(ctx, "lambdaPolicyAttachment", &iam.RolePolicyAttachmentArgs{
+			Role:      lambdaRole.Name,
+			PolicyArn: pulumi.String(policy.Arn),
+		})
+		if err != nil {
+			return err
+		}
+		// if using the provided.al2023 runtime, the build file must be called bootstrap.
+		lambdaArchive, err := archive.LookupFile(ctx, &archive.LookupFileArgs{
+			Type:       "zip",
+			SourceFile: pulumi.StringRef("./hello-world-lambda/bootstrap"),
+			OutputPath: "function.zip",
+		})
+		if err != nil {
+			return err
+		}
+		// Create a Lambda function
+		goLambdaFunction, err := lambda.NewFunction(ctx, "goLambdaFunction", &lambda.FunctionArgs{
+			Code:    pulumi.NewFileArchive(lambdaArchive.OutputPath),
+			Name:    pulumi.String("go-lambda-function"),
+			Role:    lambdaRole.Arn,
+			Handler: pulumi.String("main"),
+			Runtime: pulumi.String("provided.al2023"),
+		})
+		if err != nil {
+			return err
+		}
+
 		// Export the name of the bucket
 		ctx.Export("bucketName", bucket.ID())
 		ctx.Export("ssmName", ssmParm.Name)
+		ctx.Export("lambdaArn", goLambdaFunction.Arn)
 		return nil
 	})
 }
